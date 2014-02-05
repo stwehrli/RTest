f9ddae7c41b8476af3313a511734d65b8b550448
stwehrli <stwehrli@gmail.com>
2014-02-05 10:47:03 +0100

    Update 01.R
    This is just another test

--- 01.R
+++ 01.R
@@ -4,3 +4,5 @@ library(igraph)
 a <- random.graph.game(20, 0.1)
 plot(a, layout=layout.fruchterman.reingold, vertex.label=NA)
 plot(a, layout=layout.fruchterman.reingold, vertex.label=NA, vertex.color="red")
+
+# Let's add some more comments...
