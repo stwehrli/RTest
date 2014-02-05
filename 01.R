9ee96feba908e6126595467482571cc7b68cefcc
Stefan Wehrli <stwehrli@gmail.com>
2014-02-05 11:01:03 +0100

    Added source_https

--- 01.R
+++ 01.R
@@ -1,8 +1,14 @@
 # Test R and GitHub
 
-library(igraph)
-a <- random.graph.game(20, 0.1)
-plot(a, layout=layout.fruchterman.reingold, vertex.label=NA)
-plot(a, layout=layout.fruchterman.reingold, vertex.label=NA, vertex.color="red")
+#library(igraph)
+#a <- random.graph.game(20, 0.1)
+#plot(a, layout=layout.fruchterman.reingold, vertex.label=NA)
+#plot(a, layout=layout.fruchterman.reingold, vertex.label=NA, vertex.color="red")
 
 # Let's add some more comments...
+
+testme <- function(graph) {
+  
+  plot(graph, layout=layout.fruchterman.reingold, vertex.label=NA)
+  
+}
---
+++ Util.R
@@ -0,0 +1,16 @@
+
+source_https <- function(url, ...) {
+  ## Function for sourcing individual R scripts from GitHub
+  ## Author: Tony Breyal
+  ## Contributions: Kay Cichini
+  ## Original URL: http://tonybreyal.wordpress.com/2011/11/24/source_https-sourcing-an-r-script-from-github/
+  ## Load required package
+  require(RCurl)
+  ## Parse and evaluate each .R script
+  sapply(c(url, ...), function(u) {
+    eval(parse(text = getURL(u, followlocation = TRUE,
+                             cainfo = system.file("CurlSSL", "cacert.pem",
+                                                  package = "RCurl"))),
+         envir = .GlobalEnv)
+  })
+}
